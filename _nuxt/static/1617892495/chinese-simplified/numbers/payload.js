da32091968c263e03f696ba6b682c436fd297f1f
Xavier Champoux <39424362+LeVieuxSinge@users.noreply.github.com>
2021-04-08 10:35:11 -0400

    ed5794e

---
+++ _nuxt/static/1617892495/chinese-simplified/numbers/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/chinese-simplified/numbers", {data:[{}],fetch:[],mutations:void 0});
---
+++ _nuxt/static/1617892495/chinese-simplified/week-10/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/chinese-simplified/week-10", {data:[{}],fetch:[],mutations:void 0});
